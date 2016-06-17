2c1f002c5199cc1812c59b09639bb74069c96f14
Maarja Laineste <maarjalaineste@Maarjas-MacBook-Pro.local>
2016-06-17 09:52:52 -0700

    add Pizza prototype for calculating the price and UI logic for displaying the results

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,31 @@ function Pizza(pizzaSize, pizzaToppings) {
   this.price = 17;
 }
 
+Pizza.prototype.pizzaPrice = function() {
+  if (this.pizzaSize === "Medium") {
+    return this.price -= 2;
+  } else if (this.pizzaSize === "Small") {
+    return this.price -= 6;
+  } else
+  return this.price;
+}
+
 $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
-
+    $("ul").empty();
     var pizzaSize = $("select#pizza-size").val();
     var pizzaToppings = [];
        $(':checkbox:checked').each(function(i){
          pizzaToppings[i] = $(this).val();
     });
     var newPizza = new Pizza(pizzaSize, pizzaToppings);
-    console.log(newPizza.pizzaSize);
-    console.log(newPizza.pizzaToppings);
-    console.log(newPizza.price);
+    newPizza.pizzaPrice();
+    pizzaToppings.forEach(function(topping) {
+      $(".pizza-topping").append("<li>" + topping + "</li>");
+    });
+    $(".pizza-size").text(newPizza.pizzaSize.toLowerCase());
+    $(".pizza-price").text(newPizza.price);
+    $("#result").show();
   });
 });
